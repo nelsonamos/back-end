4259c380c1eb9d6a159d4fecfadcfcda4caf9b08
nelson amos <nelsonamos52@gmail.com>
2024-11-08 13:29:41 +0400

    static file middleware that returns lesson images

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const { MongoClient } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
+const logger = require('./logger');
 
 const app = express();
+app.set('trust proxy', true);
+app.use(logger);
 app.use(cors());
 app.use(express.json()); // Middleware to parse JSON bodies
+app.use('/middleware_pix/lesson', express.static(path.join(__dirname, '/middleware_pix/lesson')));
+app.use('/images', (req, res) => {
+    res.status(404).send('Image not found');
+});
 
 const uri = 'mongodb+srv://user:user@cluster0.cyzu5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
---
+++ logger.js
@@ -0,0 +1,12 @@
+
+const logger = (req, res, next) => {
+    const ip = req.ip; 
+    const start = Date.now();
+    res.on('finish', () => {
+        const duration = Date.now() - start;
+        console.log(`${req.method} ${req.originalUrl}   ${res.statusCode} - ${duration}ms from IP ${ip}`);
+    });
+    next();
+};
+
+module.exports = logger;
